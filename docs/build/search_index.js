f8afd782871c3791567304930bee108ae6f25dad
Valerio Losito <lositovalerio93@gmail.com>
2020-09-09 22:00:56 +0200

    rimossa build cartella

--- docs/build/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#ExampleValMas-Qualcosa","page":"-","title":"ExampleValMas Qualcosa","text":"","category":"section"},{"location":"","page":"-","title":"-","text":"Progetto di Massimo Rosselli e Valerio Losito","category":"page"}]
-}
