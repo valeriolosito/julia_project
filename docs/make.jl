6427af683531a17bd0e40582aab13522d83d1182
Valerio Losito <lositovalerio93@gmail.com>
2020-09-09 23:28:39 +0200

    Speramo

--- docs/make.jl
+++ docs/make.jl
@@ -2,5 +2,5 @@ using Documenter, Example
 
 makedocs(sitename = "My Documentation")
 deploydocs(
-    repo = "github.com/lositovalerio93@gmail.com/Example.jl.git",
+    repo = "github.com/valeriolosito/julia_project.jl.git",
 )
