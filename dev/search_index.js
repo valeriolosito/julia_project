d7f27141fe63019874a331eaf67a9ce3eafbaab2
Documenter.jl <documenter@juliadocs.github.io>
2020-10-07 17:24:04 +0000

    build based on eb2fd8d

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Progetto-Julia","page":"-","title":"Progetto Julia","text":"","category":"section"},{"location":"","page":"-","title":"-","text":"Progetto di Massimo Rosselli e Valerio Losito","category":"page"}]
+[{"location":"#Progetto-Julia","page":"-","title":"Progetto Julia","text":"","category":"section"},{"location":"","page":"-","title":"-","text":"Progetto di Massimo Rosselli e Valerio Losito","category":"page"},{"location":"funzioni/","page":"Funzioni","title":"Funzioni","text":"createM2","category":"page"}]
 }
