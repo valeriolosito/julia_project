8d76d2ff3403700bbdcf8248e5ad9b10ad380d77
Documenter.jl <documenter@juliadocs.github.io>
2020-10-07 17:10:01 +0000

    build based on f5c36ad

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#ExampleValMas-Qualcosa","page":"-","title":"ExampleValMas Qualcosa","text":"","category":"section"},{"location":"","page":"-","title":"-","text":"Progetto di Massimo Rosselli e Valerio Losito","category":"page"}]
+[{"location":"#Progetto-Julia","page":"-","title":"Progetto Julia","text":"","category":"section"},{"location":"","page":"-","title":"-","text":"Progetto di Massimo Rosselli e Valerio Losito","category":"page"}]
 }
